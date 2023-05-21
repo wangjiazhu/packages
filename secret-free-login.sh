f61cd262d55f6abab3738bb215dc570a17323422
wangjiazhu <1072002783@qq.com>
2023-05-21 20:47:31 +0800

    免密登录脚本

--- secret-free-login.sh
+++ secret-free-login.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source /etc/init.d/functions 
 
-# install the sshpass
+# 安装sshpass软件
 yum install -y sshpass
 if [ $? -eq 0 ];then
     action "sshpass install success!" /bin/true
@@ -10,26 +10,29 @@ else
     exit 1
 fi
 
-# create the key-pair
+# 创建密钥对
 ssh-keygen -f ~/.ssh/id_rsa -P '' -q
 if [ $? -eq 0 ];then
-    action "key-pair create success!" /bin/true
+    action "密钥对创建成功！" /bin/true
 else
-    action "key-pair create failed!" /bin/false
-    exit 2
+    action "密钥对创建失败！" /bin/false
+    exit 1
 fi
 
-# input the remote hosts ip
-read -p "Please input the romote host ip_address: " IP_LIST
+# 输入远程主机IP地址（可输入多个IP地址）
+read -p "请输入需免密登录的主机IP: " HOST_IP
 
-# distribute public key files to hosts
-for ip in $IP_LIST
+# 输入远程主机root密码
+read -p "请输入需要免密登录的主机root密码：" HOST_PASSWORD
+
+# 将公钥文件分发给远程主机
+for ip in $HOST_IP
 do
-    sshpass -p111111 ssh-copy-id -f -i ~/.ssh/id_rsa.pub "-o StrictHostKeyChecking=no" root@$ip
+    sshpass -p$HOST_PASSWORD ssh-copy-id -f -i ~/.ssh/id_rsa.pub "-o StrictHostKeyChecking=no" root@$ip
     if [ $? -eq 0 ];then
-        action "copy id_rsa.pub to $ip success!" /bin/true
+        action "分发公钥文件 id_rsa.pub 至 $ip 成功！" /bin/true
     else
-        action "copy id_rsa.pub to $ip failed!" /bin/false
+        action "分发公钥文件 id_rsa.pub 至 $ip 失败！" /bin/false
         continue
     fi
 done
